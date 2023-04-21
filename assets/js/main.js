408434b85de1b2db311fd531a8ba63bdad5ec361
gluke0 <milanesigianluca@gmail.com>
2023-04-21 14:37:25 +0200

    bozza API

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,15 @@ createApp({
         }
     },
     methods:{
+        generator(){
+            for (){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    let email = response.data.response;
+                    this.emails.unshift(email);
+                })
+            }
+        }
   
     }
 }).mount('#app')
